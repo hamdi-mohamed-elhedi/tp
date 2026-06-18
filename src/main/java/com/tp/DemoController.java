c6eab245f04a03323c459994a3c8c73482203291
hamdi-mohamed-elhedi <mohammed.elhedi.hamdi@gmail.com>
2026-06-18 15:44:30 +0200

    Update header text in DemoController

--- src/main/java/com/tp/DemoController.java
+++ src/main/java/com/tp/DemoController.java
@@ -13,7 +13,7 @@ public String message() {
         <html>
             <body>
                 <h1 style='color:green; text-align:center'>
-                 ✅  Demo Mercredi ✅  Test
+                 ✅  Demo du Jeudi ✅  
                 </h1>
             </body>
         </html>
