fcb76aa7dde777c6138bbefc7272c28b91488ede
hamdi-mohamed-elhedi <mohammed.elhedi.hamdi@gmail.com>
2026-06-18 13:50:56 +0200

    Update DemoController.java

--- src/main/java/com/tp/DemoController.java
+++ src/main/java/com/tp/DemoController.java
@@ -13,7 +13,7 @@ public String message() {
         <html>
             <body>
                 <h1 style='color:green; text-align:center'>
-                 ✅  Demo Mercredi ✅  tttttttttt
+                 ✅  Demo Mercredi ✅  Oumar 
                 </h1>
             </body>
         </html>
